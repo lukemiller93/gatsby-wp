1af83321ce6e6c9c53404f72538f47d7fd005aa0
lukemiller93 <lukem1293@gmail.com>
2018-12-12 23:57:18 -0600

    added image support for featured_image

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,43 +1,34 @@
-import React, {
-  Component
-} from 'react'
-import {
-  graphql
-} from 'gatsby'
+import React, { Component } from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import Image from 'gatsby-image'
 // import PostTemplate from './post'
 
 class PageTemplate extends Component {
   render() {
     // const siteMetadata = this.props.data.site.siteMetadata
     const currentPage = this.props.data.wordpressPage
-
-    return ( <
-      Layout >
-      <
-      div >
-      <
-      h1 dangerouslySetInnerHTML = {
-        {
-          __html: currentPage.title
-        }
-      }
-      /> <
-      p dangerouslySetInnerHTML = {
-        {
-          __html: currentPage.content || currentPage.acf.page_body
-        }
-      }
-      /> <
-      /div> <
-      /Layout>
+    const image = this.props.data.wordpressPage.featured_media.localFile
+    console.log(image)
+    return (
+      <Layout>
+        <div>
+          <h1 dangerouslySetInnerHTML={{ __html: currentPage.title }} />
+          {image !== null && <Image fluid={image.childImageSharp.fluid} />}
+          <p
+            dangerouslySetInnerHTML={{
+              __html: currentPage.content || currentPage.acf.page_body,
+            }}
+          />
+        </div>
+      </Layout>
     )
   }
 }
 
 export default PageTemplate
 
-export const pageQuery = graphql `
+export const pageQuery = graphql`
   query currentPageQuery($id: String!) {
     wordpressPage(id: { eq: $id }) {
       title
@@ -46,6 +37,15 @@ export const pageQuery = graphql `
       date(formatString: "MMMM DD, YYYY")
       acf {
         page_body
+      }
+      featured_media {
+        localFile {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
       }
     }
     site {
